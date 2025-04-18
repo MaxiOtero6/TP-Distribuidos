78f49e31568529e626a9fd0977bc393cc4bb6fa2
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-18 18:07:07 -0300

    feat: queue struct

---
+++ mom/queue.go
@@ -0,0 +1,90 @@
+package mom
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type queue struct {
+	channel     *amqp.Channel
+	name        string
+	amqpName    string
+	durable     bool
+	autoDeleted bool
+	exclusive   bool
+	noWait      bool
+	args        []string
+}
+
+func newQueue(ch *amqp.Channel, name string) *queue {
+	q := &queue{
+		channel:     ch,
+		name:        name,
+		durable:     true,
+		autoDeleted: false,
+		exclusive:   false,
+		noWait:      false,
+		args:        nil,
+	}
+
+	amqpQ, err := ch.QueueDeclare(
+		q.name,
+		q.durable,
+		q.autoDeleted,
+		q.exclusive,
+		q.noWait,
+		nil,
+	)
+
+	q.amqpName = amqpQ.Name
+
+	failOnError(err, fmt.Sprintf("Failed to declare the queue: '%v'", name))
+
+	return q
+}
+
+func (q *queue) bind(exchangeName string, routingKey string) {
+	err := q.channel.QueueBind(
+		q.amqpName,
+		routingKey,
+		exchangeName,
+		q.noWait,
+		nil,
+	)
+
+	failOnError(err, fmt.Sprintf("Failed to bind the queue: '%v'", q.amqpName))
+}
+
+func (q *queue) consume() <-chan amqp.Delivery {
+	msgs, err := q.channel.Consume(
+		q.amqpName,
+		q.amqpName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, fmt.Sprintf("Failed to register a consumer: '%v'", q.amqpName))
+
+	go func() {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGTERM)
+		<-signalChan
+
+		log.Infof("Received SIGTERM signal, cancelling consumer '%v'", q.name)
+
+		if err := q.channel.Cancel(q.amqpName, false); err != nil {
+			log.Errorf("Failed to cancel the consumer '%v': '%v'", q.name, err)
+		} else {
+			log.Infof("Consumer '%v' cancelled gracefully ", q.name)
+		}
+	}()
+
+	return msgs
+}
