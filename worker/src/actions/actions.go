d5819c4135d2ab3f61d7d01aee63cdd1fa2fdf8c
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-19 18:29:01 -0300

    feat: actions consts

--- worker/src/actions/actions.go
+++ worker/src/actions/actions.go
@@ -7,12 +7,30 @@ import (
 
 var log = logging.MustGetLogger("log")
 
+type Tasks map[string]map[string]map[string]*protocol.Task
+
 type Action interface {
 	// Execute executes the action.
 	// It returns a map of tasks for the next stages.
 	// It returns an error if the action fails.
-	Execute(task *protocol.Task) (map[string]*protocol.Task, error)
+	Execute(task *protocol.Task) (Tasks, error)
 }
+
+const FILTER_EXCHANGE string = "filterExchange"
+const JOIN_EXCHANGE string = "joinExchange"
+const RESULT_EXCHANGE string = "resultExchange"
+const MAP_EXCHANGE string = "mapExchange"
+const BROADCAST_ID string = ""
+
+const BETA_STAGE string = "beta"
+const ZETA_STAGE string = "zeta"
+const IOTA_STAGE string = "iota"
+const DELTA_STAGE string = "delta"
+const NU_STAGE string = "nu"
+const RESULT_STAGE string = "result"
+
+const TEST_WORKER_COUNT int = 1
+const TEST_WORKER_ID string = "0"
 
 type ActionType string
 
