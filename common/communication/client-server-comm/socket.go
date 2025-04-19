9f5a1e0467fc44a73cb43c79d29bd9f2401cd745
Tinchocw <mcwikla@fi.uba.ar>
2025-04-19 13:17:52 -0300

    refactor: send length and message

--- common/communication/client-server-comm/socket.go
+++ common/communication/client-server-comm/socket.go
@@ -5,10 +5,11 @@ import (
 	"net"
 
 	"github.com/MaxiOtero6/TP-Distribuidos/common/communication/client-server-comm/protocol"
+	"github.com/op/go-logging"
 	"google.golang.org/protobuf/proto"
 )
 
-const COMMUNICATION_DELIMITER = '\n'
+var log = logging.MustGetLogger("log")
 
 type Socket struct {
 	conn     net.Conn
@@ -61,41 +62,72 @@ func (s *Socket) Accept() (*Socket, error) {
 }
 
 func (s *Socket) Read() (*protocol.Message, error) {
-	message, err := s.reader.ReadBytes(COMMUNICATION_DELIMITER)
+
+	lengthBytes := make([]byte, 2)
+	_, err := s.reader.Read(lengthBytes)
 	if err != nil {
+		log.Errorf("Error reading message length: %v", err)
 		return nil, err
 	}
 
-	message = message[:len(message)-1]
+	length := int(lengthBytes[0])<<8 | int(lengthBytes[1])
+	log.Debugf("Message length: %d", length)
 
+	message := make([]byte, length)
+
+	messageReceivedLength := 0
+
+	for messageReceivedLength < length {
+		n, err := s.reader.Read(message[messageReceivedLength:])
+		if err != nil {
+			log.Errorf("Error reading message: %v", err)
+			return nil, err
+		}
+		messageReceivedLength += n
+	}
+
+	// Deserializa el mensaje usando proto.Unmarshal
 	var responseMessage protocol.Message
 	err = proto.Unmarshal(message, &responseMessage)
 	if err != nil {
+		log.Errorf("Error deserializing message: %v", err)
 		return nil, err
 	}
 
+	log.Debugf("Successfully deserialized message: %v", responseMessage)
 	return &responseMessage, nil
 }
 
 func (s *Socket) Write(message *protocol.Message) error {
+
+	log.Info("Writing message to socket")
+	log.Infof("Message: %v", message)
 	message_bytes, err := proto.Marshal(message)
 	if err != nil {
 		return err
 	}
 
-	message_bytes = append(message_bytes, byte(COMMUNICATION_DELIMITER))
+	log.Debugf("Sending message: %v", message_bytes)
+
+	length := len(message_bytes)
+
+	sendMessage := make([]byte, 2+length)
+
+	// Write the length of the message in the first 2 bytes
+	sendMessage[0] = byte(length >> 8)
+	sendMessage[1] = byte(length & 0xFF)
+
+	copy(sendMessage[2:], message_bytes)
 
 	bytes_written := 0
-
-	for bytes_written < len(message_bytes) {
-		n, err := s.conn.Write(message_bytes[bytes_written:])
+	for bytes_written < len(sendMessage) {
+		n, err := s.conn.Write(sendMessage[bytes_written:])
 		if err != nil {
-
+			log.Errorf("Error writing to socket: %v", err)
 			return err
 		}
 		bytes_written += n
 	}
-
 	return nil
 }
 
