4815112b0e7051a269a9a1a9d9fdc2fbea03bb99
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-20 14:23:40 -0300

    refactor: return slice instead of matrix

--- server/src/server/service.go
+++ server/src/server/service.go
@@ -1,79 +1,46 @@
 package server
 
 import (
-	"fmt"
-
 	"github.com/MaxiOtero6/TP-Distribuidos/common/communication/client-server-comm/protocol"
 	"github.com/MaxiOtero6/TP-Distribuidos/server/src/model"
 	"github.com/MaxiOtero6/TP-Distribuidos/server/src/utils"
 )
 
-
-func (s *Server) processMovieBatch(batch *protocol.Batch) ([][]*model.Movie, error) {
-	var movies [][]*model.Movie
-
-	for _, row := range batch.Data {
-
-		fields := utils.ParseLine(&row.Data)
-		if fields == nil {
-			return nil, fmt.Errorf("Invalid row data")
-		}
-
-		movie := utils.ParseMovie(fields)
-		if movie == nil {
-			return nil, fmt.Errorf("Invalid movie data")
-		}
-		movies = append(movies, movie)
-
-	}
-	return movies, nil
-}
-
-func (s *Server) processCreditBatch(batch *protocol.Batch) ([][]*model.Actor, error) {
-	var actors [][]*model.Actor
+func (s *Server) processMoviesBatch(batch *protocol.Batch) []*model.Movie {
+	var ret []*model.Movie
 
 	for _, row := range batch.Data {
-
 		fields := utils.ParseLine(&row.Data)
-		if fields == nil {
-			return nil, fmt.Errorf("Invalid row data")
-		}
+		movies := utils.ParseMovie(fields)
 
-		actor := utils.ParseCredit(fields)
-		if actor == nil {
-			return nil, fmt.Errorf("Invalid actor data")
-		}
-		actors = append(actors, actor)
-
+		ret = append(ret, movies...)
 	}
-	return actors, nil
+
+	return ret
 }
 
-func (s *Server) processRatingsBatch(batch *protocol.Batch) ([][]*model.Rating, error) {
-	var ratings [][]*model.Rating
+func (s *Server) processCreditsBatch(batch *protocol.Batch) []*model.Actor {
+	var ret []*model.Actor
 
 	for _, row := range batch.Data {
-
 		fields := utils.ParseLine(&row.Data)
-		if fields == nil {
-			return nil, fmt.Errorf("Invalid row data")
-		}
+		actors := utils.ParseCredit(fields)
 
-		rating := utils.ParseRating(fields)
-		if rating == nil {
-			return nil, fmt.Errorf("Invalid rating data")
-		}
-		ratings = append(ratings, rating)
-
+		ret = append(ret, actors...)
 	}
-	return ratings, nil
+
+	return ret
 }
 
-func (s *Server) validateBatchType(batchMessage *protocol.Batch) error {
-	if batchMessage.Type != protocol.FileType_MOVIES &&
-		batchMessage.Type != protocol.FileType_CREDITS &&
-		batchMessage.Type != protocol.FileType_RATINGS {
-		return fmt.Errorf("Invalid file type: %v", batchMessage.Type)
+func (s *Server) processRatingsBatch(batch *protocol.Batch) []*model.Rating {
+	var ret []*model.Rating
+
+	for _, row := range batch.Data {
+		fields := utils.ParseLine(&row.Data)
+		ratings := utils.ParseRating(fields)
+
+		ret = append(ret, ratings...)
 	}
-	return nil
+
+	return ret
 }
