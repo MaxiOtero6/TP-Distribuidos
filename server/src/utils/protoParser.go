72fe794fd96b9ae838497c41779beb4fc6e2d984
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-20 15:50:04 -0300

    feat: parse model to task

---
+++ server/src/utils/protoParser.go
@@ -0,0 +1,126 @@
+package utils
+
+import (
+	"github.com/MaxiOtero6/TP-Distribuidos/common/communication/server-comm/protocol"
+	"github.com/MaxiOtero6/TP-Distribuidos/common/utils"
+	"github.com/MaxiOtero6/TP-Distribuidos/server/src/model"
+)
+
+func GetAlphaStageTask(movies []*model.Movie) (tasks map[string]*protocol.Task) {
+	tasks = make(map[string]*protocol.Task)
+	var alphaData []*protocol.Alpha_Data
+
+	for _, movie := range movies {
+		alphaData = append(alphaData, &protocol.Alpha_Data{
+			Id:            movie.Id,
+			Title:         movie.Title,
+			ProdCountries: movie.ProdCountries,
+			Genres:        movie.Genres,
+			ReleaseYear:   movie.ReleaseYear,
+		})
+	}
+
+	tasks[""] = &protocol.Task{
+		Stage: &protocol.Task_Alpha{
+			Alpha: &protocol.Alpha{
+				Data: alphaData,
+			},
+		},
+	}
+
+	return tasks
+}
+
+func GetZetaStageRatingsTask(ratings []*model.Rating, joinersCount int) (tasks map[string]*protocol.Task) {
+	tasks = make(map[string]*protocol.Task)
+	zetaData := make(map[string][]*protocol.Zeta_Data)
+
+	for _, rating := range ratings {
+		idHash, err := utils.GetWorkerIdFromHash(joinersCount, rating.MovieId)
+
+		if err != nil {
+			continue
+		}
+
+		zetaData[idHash] = append(zetaData[idHash], &protocol.Zeta_Data{
+			Data: &protocol.Zeta_Data_Rating_{
+				Rating: &protocol.Zeta_Data_Rating{
+					MovieId: rating.MovieId,
+					Rating:  rating.Rating,
+				},
+			},
+		})
+	}
+
+	for id, data := range zetaData {
+		tasks[id] = &protocol.Task{
+			Stage: &protocol.Task_Zeta{
+				Zeta: &protocol.Zeta{
+					Data: data,
+				},
+			},
+		}
+	}
+
+	return tasks
+}
+
+func GetIotaStageCreditsTask(actors []*model.Actor, joinersCount int) (tasks map[string]*protocol.Task) {
+	tasks = make(map[string]*protocol.Task)
+	iotaData := make(map[string][]*protocol.Iota_Data)
+
+	for _, actor := range actors {
+		idHash, err := utils.GetWorkerIdFromHash(joinersCount, actor.MovieId)
+
+		if err != nil {
+			continue
+		}
+
+		iotaData[idHash] = append(iotaData[idHash], &protocol.Iota_Data{
+			Data: &protocol.Iota_Data_Actor_{
+				Actor: &protocol.Iota_Data_Actor{
+					Id:      actor.Id,
+					Name:    actor.Name,
+					MovieId: actor.MovieId,
+				},
+			},
+		})
+	}
+
+	for id, data := range iotaData {
+		tasks[id] = &protocol.Task{
+			Stage: &protocol.Task_Iota{
+				Iota: &protocol.Iota{
+					Data: data,
+				},
+			},
+		}
+	}
+
+	return tasks
+}
+
+func GetMuStageTask(movies []*model.Movie) (tasks map[string]*protocol.Task) {
+	tasks = make(map[string]*protocol.Task)
+	var muData []*protocol.Mu_Data
+
+	for _, movie := range movies {
+		muData = append(muData, &protocol.Mu_Data{
+			Id:       movie.Id,
+			Title:    movie.Title,
+			Revenue:  movie.Revenue,
+			Budget:   movie.Budget,
+			Overview: movie.Overview,
+		})
+	}
+
+	tasks[""] = &protocol.Task{
+		Stage: &protocol.Task_Mu{
+			Mu: &protocol.Mu{
+				Data: muData,
+			},
+		},
+	}
+
+	return tasks
+}
