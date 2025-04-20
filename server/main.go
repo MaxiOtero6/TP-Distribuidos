f50c6297e23e247638b4d0ef21e9c01cbca6f464
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-20 13:06:47 -0300

    feat: init rabbitmq config

--- server/main.go
+++ server/main.go
@@ -7,12 +7,16 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/MaxiOtero6/TP-Distribuidos/common/model"
+	"github.com/MaxiOtero6/TP-Distribuidos/common/utils"
 	server "github.com/MaxiOtero6/TP-Distribuidos/server/src"
 	"github.com/op/go-logging"
 	"github.com/spf13/viper"
 )
 
 var log = logging.MustGetLogger("log")
+
+const NODE_TYPE = "SERVER"
 
 // InitConfig Function that uses viper library to parse configuration parameters.
 // Viper is configured to read variables from both environment variables and the
@@ -77,6 +81,38 @@ func handleSigterm(signalChan chan os.Signal, server *server.Server) {
 	)
 }
 
+func initServer(v *viper.Viper) *server.Server {
+	address := v.GetString("address")
+	id := v.GetString("id")
+
+	clusterConfig := &model.WorkerClusterConfig{
+		FilterCount:   v.GetInt("filter.count"),
+		OverviewCount: v.GetInt("overview.count"),
+		MapCount:      v.GetInt("map.count"),
+		JoinCount:     v.GetInt("join.count"),
+		ReduceCount:   v.GetInt("reduce.count"),
+		TopCount:      v.GetInt("top.count"),
+	}
+
+	exchanges, queues, binds, err := utils.GetRabbitConfig(NODE_TYPE, v)
+
+	if err != nil {
+		log.Panicf("Failed to parse RabbitMQ configuration: %s", err)
+	}
+
+	s, err := server.NewServer(id, address, clusterConfig)
+
+	if err != nil {
+		log.Panicf("Failed to initialize server: %s", err)
+	}
+
+	s.InitConfig(exchanges, queues, binds)
+
+	log.Infof("Server %v ready", s.ServerId)
+
+	return s
+}
+
 func main() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGTERM)
@@ -90,12 +126,7 @@ func main() {
 		log.Criticalf("%s", err)
 	}
 
-	address := v.GetString("address")
-	s, err := server.NewServer(address)
-	if err != nil {
-		log.Criticalf("Failed to initialize server: %s", err)
-		os.Exit(1)
-	}
+	s := initServer(v)
 
 	go handleSigterm(signalChan, s)
 
