19a672ff241c1b534e84619f10d56a384ca344ce
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-04-21 01:23:55 -0300

    feat: mapper implementation

--- worker/src/actions/mapper.go
+++ worker/src/actions/mapper.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/MaxiOtero6/TP-Distribuidos/common/communication/server-comm/protocol"
 	"github.com/MaxiOtero6/TP-Distribuidos/common/model"
+	"github.com/MaxiOtero6/TP-Distribuidos/common/utils"
 )
 
 // Mapper is a struct that implements the Action interface.
@@ -45,13 +46,26 @@ func (m *Mapper) delta1Stage(data []*protocol.Delta_1_Data) (tasks Tasks) {
 	tasks[REDUCE_EXCHANGE][DELTA_STAGE_2] = make(map[string]*protocol.Task)
 	delta2Data := make(map[string][]*protocol.Delta_2_Data)
 
-	log.Panicf("Mapper: Delta_1 stage not implemented yet %v", data)
+	dataMap := make(map[string]*protocol.Delta_2_Data)
 
-	// TODO: process data
-	// TODO: see filter.go or overviewer.go for examples
-	// for _, movie := range data {
+	for _, movie := range data {
+		prodCountry := movie.GetCountry()
 
-	// }
+		if _, ok := dataMap[prodCountry]; !ok {
+			dataMap[prodCountry] = &protocol.Delta_2_Data{
+				Country:       prodCountry,
+				PartialBudget: 0,
+			}
+		}
+
+		dataMap[prodCountry].PartialBudget += movie.GetBudget()
+	}
+
+	for _, e2Data := range dataMap {
+		idHash := utils.RandomHash(m.infraConfig.GetReduceCount())
+
+		delta2Data[idHash] = append(delta2Data[idHash], e2Data)
+	}
 
 	for id, data := range delta2Data {
 		tasks[REDUCE_EXCHANGE][DELTA_STAGE_2][id] = &protocol.Task{
@@ -91,13 +105,33 @@ func (m *Mapper) eta1Stage(data []*protocol.Eta_1_Data) (tasks Tasks) {
 	tasks[REDUCE_EXCHANGE][ETA_STAGE_2] = make(map[string]*protocol.Task)
 	eta2Data := make(map[string][]*protocol.Eta_2_Data)
 
-	log.Panicf("Mapper: Eta_1 stage not implemented yet %v", data)
+	dataMap := make(map[string]*protocol.Eta_2_Data)
 
-	// TODO: process data
-	// TODO: see filter.go or overviewer.go for examples
-	// for _, movie := range data {
+	for _, movieRating := range data {
+		movieId := movieRating.GetId()
 
-	// }
+		if _, ok := dataMap[movieId]; !ok {
+			dataMap[movieId] = &protocol.Eta_2_Data{
+				Id:     movieId,
+				Title:  movieRating.GetTitle(),
+				Rating: 0.0,
+				Count:  0,
+			}
+		}
+
+		dataMap[movieId].Rating += float64(movieRating.GetRating())
+		dataMap[movieId].Count += 1
+	}
+
+	for movieId, e2Data := range dataMap {
+		idHash, err := utils.GetWorkerIdFromHash(m.infraConfig.GetReduceCount(), movieId)
+		if err != nil {
+			log.Errorf("Mapper: error getting worker id from hash %v", err)
+			continue
+		}
+
+		eta2Data[idHash] = append(eta2Data[idHash], e2Data)
+	}
 
 	for id, data := range eta2Data {
 		tasks[REDUCE_EXCHANGE][ETA_STAGE_2][id] = &protocol.Task{
@@ -137,13 +171,31 @@ func (m *Mapper) kappa1Stage(data []*protocol.Kappa_1_Data) (tasks Tasks) {
 	tasks[REDUCE_EXCHANGE][KAPPA_STAGE_2] = make(map[string]*protocol.Task)
 	kappa2Data := make(map[string][]*protocol.Kappa_2_Data)
 
-	log.Panicf("Mapper: Kappa_1 stage not implemented yet %v", data)
+	dataMap := make(map[string]*protocol.Kappa_2_Data)
 
-	// TODO: process data
-	// TODO: see filter.go or overviewer.go for examples
-	// for _, movie := range data {
+	for _, actor := range data {
+		actorId := actor.GetActorId()
 
-	// }
+		if _, ok := dataMap[actorId]; !ok {
+			dataMap[actorId] = &protocol.Kappa_2_Data{
+				ActorId:               actorId,
+				ActorName:             actor.GetActorName(),
+				PartialParticipations: 0,
+			}
+		}
+
+		dataMap[actorId].PartialParticipations += 1
+	}
+
+	for actorId, k2Data := range dataMap {
+		idHash, err := utils.GetWorkerIdFromHash(m.infraConfig.GetReduceCount(), actorId)
+		if err != nil {
+			log.Errorf("Mapper: error getting worker id from hash %v", err)
+			continue
+		}
+
+		kappa2Data[idHash] = append(kappa2Data[idHash], k2Data)
+	}
 
 	for id, data := range kappa2Data {
 		tasks[REDUCE_EXCHANGE][KAPPA_STAGE_2][id] = &protocol.Task{
@@ -183,13 +235,40 @@ func (m *Mapper) nu1Stage(data []*protocol.Nu_1_Data) (tasks Tasks) {
 	tasks[REDUCE_EXCHANGE][NU_STAGE_2] = make(map[string]*protocol.Task)
 	nu2Data := make(map[string][]*protocol.Nu_2_Data)
 
-	log.Panicf("Mapper: Nu_1 stage not implemented yet %v", data)
+	for _, movie := range data {
+		idHash, err := utils.GetWorkerIdFromHash(m.infraConfig.GetReduceCount(), movie.GetId())
+		if err != nil {
+			log.Errorf("Mapper: error getting worker id from hash %v", err)
+			continue
+		}
 
-	// TODO: process data
-	// TODO: see filter.go or overviewer.go for examples
-	// for _, movie := range data {
+		// Initialize idHash key if it doesn't exist
+		if _, ok := nu2Data[idHash]; !ok {
+			nu2Data[idHash] = []*protocol.Nu_2_Data{
+				{
+					Sentiment: false,
+					Revenue:   0,
+					Budget:    0,
+					Count:     0,
+				},
+				{
+					Sentiment: true,
+					Revenue:   0,
+					Budget:    0,
+					Count:     0,
+				},
+			}
+		}
 
-	// }
+		var sentiment_index int = 0
+		if movie.GetSentiment() {
+			sentiment_index = 1
+		}
+
+		nu2Data[idHash][sentiment_index].Revenue += movie.GetRevenue()
+		nu2Data[idHash][sentiment_index].Budget += movie.GetBudget()
+		nu2Data[idHash][sentiment_index].Count += 1
+	}
 
 	for id, data := range nu2Data {
 		tasks[REDUCE_EXCHANGE][NU_STAGE_2][id] = &protocol.Task{
